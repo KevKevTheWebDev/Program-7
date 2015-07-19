cb0023cc6ec396087ec5e13957ab46a86c8f5345
KevKevTheWebDev <student.122454@worc.k12.ma.us>
2015-07-19 19:40:00 -0400

    Update and rename README.md to PeopleCounter.java

---
+++ PeopleCounter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class apples{
+	public static void main(String args[]){
+	Scanner UserTyped = new Scanner(System.in);
+	
+	int girls,boys,people;
+	girls = 5;
+	boys = 5;
+	people = girls+boys;
+	System.out.println(people);
+	}
+}
